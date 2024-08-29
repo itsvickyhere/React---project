f59e97589ce0d61328dec7793b016623817eb034
itsvickyhere <vigneshbhaskaran18@gmail.com>
2024-08-29 16:07:32 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
